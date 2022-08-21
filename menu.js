bf816908375cac38c8fd6ba05d2683da562ec84a
PaulT <ptimofejev@gmail.com>
2022-08-21 19:46:54 +0300

    three lessons completed

---
+++ menu.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const nav = document.querySelector('.nav');
+const toggler = document.querySelector('.nav__toggler');
+
+toggler.addEventListener('click', function () {
+  nav.classList.toggle('nav--open');
+});
